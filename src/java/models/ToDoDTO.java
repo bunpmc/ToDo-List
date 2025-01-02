8c233271331122ce2108798fbe2ed4847970b254
Lu.Bu <phamhoangminhchau1973@gmail.com>
2025-01-02 22:06:19 +0700

    First push

---
+++ src/java/models/ToDoDTO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Minh Châu
+ */
+public class ToDoDTO {
+    private String name;
+    private String body;
+    private String status;
+
+    public ToDoDTO(String name, String body, String status) {
+        this.name = name;
+        this.body = body;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
